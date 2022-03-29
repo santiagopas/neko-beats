a5b7b5e2a035181b03c5a3a5d610162867bfd411
santiago pasciullo <santiagopasciullo@gmail.com>
2022-03-29 02:47:39 -0300

    :rocket: subida inicial

---
+++ src/components/loadingImage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Loading = () => {
+    return (
+        <div className='loading'>
+        <div class="container">
+          <div class="ring"></div>
+          <div class="ring"></div>
+          <div class="ring"></div>
+          <p>Cargando...</p>
+        </div>
+      </div>
+    )
+}
+
+export default Loading
